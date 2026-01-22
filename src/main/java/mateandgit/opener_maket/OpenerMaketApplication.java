8f5c3fa3da3c758b2794956155ced27d5758e90a
USER <sola201@naver.com>
2026-01-22 12:59:56 -0300

    Initial commit

---
+++ src/main/java/mateandgit/opener_maket/OpenerMaketApplication.java
@@ -0,0 +1,13 @@
+package mateandgit.opener_maket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenerMaketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenerMaketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mateandgit/opener_maket/OpenerMaketApplicationTests.java
@@ -0,0 +1,13 @@
+package mateandgit.opener_maket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenerMaketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
